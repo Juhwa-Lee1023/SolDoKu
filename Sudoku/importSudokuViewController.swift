983db327a6b253394c4c2a7cd52de2c7add33369
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-09-06 22:33:03 +0900

    :tada: Start Project
    Start the Sudoku Project

---
+++ Sudoku/importSudokuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  importSudokuViewController.swift
+//  Sudoku
+//
+//  Created by 이주화 on 2022/09/06.
+//
+
+import UIKit
+
+class importSudokuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
