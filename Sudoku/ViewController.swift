ddb3c49890ca1a69361be4f0562b4f458019fe5f
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-09-06 21:56:04 +0900

    Initial Commit

---
+++ Sudoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sudoku
+//
+//  Created by 이주화 on 2022/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
