9302c9d0fc1bdf84fbd77e0b8d32b78064158afc
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-09-29 00:39:17 +0900

    폴더명 수정 및 extension 분리

---
+++ Sudoku/Extensions/CALayer.swift
@@ -0,0 +1,30 @@
+//
+//  CALayer.swift
+//  Sudoku
+//
+//  Created by 이주화 on 2022/09/29.
+//
+extension CALayer {
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+                break
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+            default: break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
+    }
+}
---
+++ Sudoku/Extensions/UIImage.swift
@@ -0,0 +1,95 @@
+//
+//  UIImage.swift
+//  Sudoku
+//
+//  Created by 이주화 on 2022/09/14.
+//
+extension UIImage {
+    // UIImage에 UIImage를 픽셀버퍼 타입으로 변환시키는 function 추가
+    func UIImageToPixelBuffer() -> CVPixelBuffer? {
+        
+        let width = Int(self.size.width)
+        let height = Int(self.size.height)
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer: CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard status == kCVReturnSuccess else {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        guard let context = CGContext(data: pixelData, width: width, height: height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {
+            return nil
+        }
+        
+        context.translateBy(x: 0, y: CGFloat(height))
+        context.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context)
+        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+    // UIImage에 UIImage의 방향을 확인하여 정방향으로 돌리는 function 추가
+    func fixOrientation() -> UIImage {
+        
+        // 이미지의 방향이 올바를 경우 수정하지 않는다.
+        if ( self.imageOrientation == UIImage.Orientation.up ) {
+            return self
+        }
+        
+        // 이미지를 변환시키기 위한 함수 선언
+        var transform: CGAffineTransform = CGAffineTransform.identity
+        
+        // 이미지의 상태에 맞게 이미지를 돌린다.
+        if ( self.imageOrientation == UIImage.Orientation.down || self.imageOrientation == UIImage.Orientation.downMirrored ) {
+            transform = transform.translatedBy(x: self.size.width, y: self.size.height)
+            transform = transform.rotated(by: CGFloat(Double.pi))
+        }
+        
+        if ( self.imageOrientation == UIImage.Orientation.left || self.imageOrientation == UIImage.Orientation.leftMirrored ) {
+            transform = transform.translatedBy(x: self.size.width, y: 0)
+            transform = transform.rotated(by: CGFloat(Double.pi / 2.0))
+        }
+        
+        if ( self.imageOrientation == UIImage.Orientation.right || self.imageOrientation == UIImage.Orientation.rightMirrored ) {
+            transform = transform.translatedBy(x: 0, y: self.size.height)
+            transform = transform.rotated(by: CGFloat(-Double.pi / 2.0))
+        }
+        
+        if ( self.imageOrientation == UIImage.Orientation.upMirrored || self.imageOrientation == UIImage.Orientation.downMirrored ) {
+            transform = transform.translatedBy(x: self.size.width, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+        }
+        
+        if ( self.imageOrientation == UIImage.Orientation.leftMirrored || self.imageOrientation == UIImage.Orientation.rightMirrored ) {
+            transform = transform.translatedBy(x: self.size.height, y: 0)
+            transform = transform.scaledBy(x: -1, y: 1)
+        }
+        
+        // 이미지 변환용 값 선언
+        let cgValue: CGContext = CGContext(data: nil, width: Int(self.size.width), height: Int(self.size.height),
+                                           bitsPerComponent: self.cgImage!.bitsPerComponent, bytesPerRow: 0,
+                                           space: self.cgImage!.colorSpace!,
+                                           bitmapInfo: self.cgImage!.bitmapInfo.rawValue)!
+        
+        cgValue.concatenate(transform)
+        
+        if ( self.imageOrientation == UIImage.Orientation.left ||
+             self.imageOrientation == UIImage.Orientation.leftMirrored ||
+             self.imageOrientation == UIImage.Orientation.right ||
+             self.imageOrientation == UIImage.Orientation.rightMirrored ) {
+            cgValue.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: self.size.height, height: self.size.width))
+        } else {
+            cgValue.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        }
+        
+        return UIImage(cgImage: cgValue.makeImage()!)
+    }
+}
