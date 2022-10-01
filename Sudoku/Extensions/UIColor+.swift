81b797fbbebe6c663b6c2116d0ff3f9d20b51fff
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-10-01 23:21:04 +0900

    컬러 추가

--- Sudoku/Extensions/UIColor+.swift
+++ Sudoku/Extensions/UIColor+.swift
@@ -5,16 +5,26 @@
 //  Created by 이주화 on 2022/09/29.
 //
 enum Colors {
-  case sudokuPuple
-  case sudokuButton
-  case sudokuRed
-  case sudokuEmpty
+    case sudokuLightPurple
+    case sudokuDeepPurple
+    case sudokuPurple
+    case sudokuButton
+    case sudokuRed
+    case sudokuEmpty
+    case sudokuDeepButton
+    case sudokuLightRed
 }
 
 extension UIColor {
     static func sudokuColor(_ color: Colors) -> UIColor {
         switch color {
-        case .sudokuPuple:
+        case .sudokuDeepButton:
+            return UIColor(red: 85/255, green: 85/255, blue: 103/255, alpha: 255/255)
+        case .sudokuLightPurple:
+            return UIColor(red: 107/255, green: 28/255, blue: 255/255, alpha: 20/100)
+        case .sudokuDeepPurple:
+            return UIColor(red: 107/255, green: 28/255, blue: 255/255, alpha: 100/100)
+        case .sudokuPurple:
             return UIColor(red: 107/255, green: 28/255, blue: 255/255, alpha: 60/100)
         case .sudokuButton:
             return UIColor(red: 217/255, green: 217/255, blue: 217/255, alpha: 100/100)
@@ -22,6 +32,8 @@ extension UIColor {
             return UIColor(red: 210/255, green: 31/255, blue: 0/255, alpha: 100/100)
         case .sudokuEmpty:
             return UIColor(red: 210/255, green: 31/255, blue: 0/255, alpha: 0/100)
+        case .sudokuLightRed:
+            return UIColor(red: 255/255, green: 28/255, blue: 107/255, alpha: 60/100)
         }
-      }
+    }
 }
