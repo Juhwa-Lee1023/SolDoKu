6878b0f3094ed2b94063f6ad44ec1461871cb9a6
Juhwa-Lee1023 <juhwa.lee1023@gmail.com>
2023-03-10 16:03:15 +0900

    fix: 스도쿠 알고리즘 개선
    1. `isVerify` 함수의 반환 값을 저장하지 않고 바로 사용
    2.  `col` 값에 1을 더하여 다음 열을 계산하는 대신, `nextCol` 변수에 다음 열의 값을 계산하고, `nextRow` 변수를 사용하여 다음 행의 값을 계산하도록 변경

--- Sudoku/sudokuCalculation.swift
+++ Sudoku/sudokuCalculation.swift
@@ -15,19 +15,19 @@ func isVerify(_ number: Int, _ sudoku: [[Int]], _ row: Int, _ col:Int) -> Bool {
     let row2 = (row + 4) % 3
     let col1 = (col + 2) % 3
     let col2 = (col + 4) % 3
-
+    
     // 들어갈 숫자가 row, column에 있는 숫자와 겹치는지 확인
     for i in 0..<9 {
         if sudoku[i][col] == number { return false }
         if sudoku[row][i] == number { return false }
     }
-
+    
     // 숫자가 들어갈 3*3의 공간에 숫자가 겹치는지 확인
     if sudoku[row1 + sectorRow][col1 + sectorCol] == number { return false }
     if sudoku[row2 + sectorRow][col1 + sectorCol] == number { return false }
     if sudoku[row1 + sectorRow][col2 + sectorCol] == number { return false }
     if sudoku[row2 + sectorRow][col2 + sectorCol] == number { return false }
-
+    
     return true
 }
 
@@ -36,31 +36,25 @@ func sudokuCalculation(_ sudoku: inout [[Int]], _ row: Int, _ col: Int, _ check:
     if(check >= 1000000) { return false }
     
     if (row == 9) { return true }
-
+    
     // 기존에 존재하는 숫자가 있다면
     if sudoku[row][col] != 0 {
-        if (col == 8) {
-            check += 1
-            if (sudokuCalculation(&sudoku, row+1, 0, &check) == true) { return true }
-        } else {
-            check += 1
-            if (sudokuCalculation(&sudoku, row, col+1, &check) == true) { return true }
-        }
-        return false
+        let nextCol = (col + 1) % 9
+        let nextRow = nextCol == 0 ? row + 1 : row
+        check += 1
+        return sudokuCalculation(&sudoku, nextRow, nextCol, &check)
     }
-
+    
     // 모든 칸을 채울 때까지 재귀함수 호출
     for num in 1..<10 {
-        if isVerify(num, sudoku, row, col) == true {
+        if isVerify(num, sudoku, row, col) {
             sudoku[row][col] = num
-            if col == 8 {
-                check += 1
-                if (sudokuCalculation(&sudoku, row+1, 0, &check) == true) { return true }
-            } else {
-                check += 1
-                if (sudokuCalculation(&sudoku, row, col+1, &check) == true) { return true }
+            let nextCol = (col + 1) % 9
+            let nextRow = nextCol == 0 ? row + 1 : row
+            check += 1
+            if sudokuCalculation(&sudoku, nextRow, nextCol, &check) {
+                return true
             }
-            // 계산이 불가능하면...
             sudoku[row][col] = 0
         }
     }
